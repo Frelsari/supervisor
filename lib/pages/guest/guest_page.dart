4ff84be0f39f3cc33f871239274f5b3e59468602
Ricky Hu <ricky9667@gmail.com>
2020-10-27 11:33:42 +0800

    Init guest page

---
+++ lib/pages/guest/guest_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Guest extends StatelessWidget {
+  static const sName = "/guest_page";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('家屬'),
+      ),
+      body: Container(),
+    );
+  }
+}
