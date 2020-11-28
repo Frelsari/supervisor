5baef2d52ff16068561bf8e7532229c2b2d98017
Ricky Hu <ricky9667@gmail.com>
2020-11-28 15:34:01 +0800

    Add auto load data for guest_page.dart

--- lib/pages/guest/guest_page.dart
+++ lib/pages/guest/guest_page.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
-class Guest extends StatelessWidget {
-  static const sName = "/guest_page";
+class Guest extends StatefulWidget {
+  @override
+  _GuestState createState() => _GuestState();
 
-  final Map _data;
+  final Map data;
 
-  Guest(Map<String, String> machineData) : _data = machineData;
+  Guest(Map<String, String> machineData) : data = machineData;
+}
+
+class _GuestState extends State<Guest> {
+  FirebaseFirestore _firestore;
+  Map _data;
+  Stream dataStream;
 
   Color getPowerColor(String s) {
     int power = int.parse(s);
@@ -21,133 +30,240 @@ class Guest extends StatelessWidget {
   }
 
   @override
+  void initState() {
+    super.initState();
+    _data = widget.data;
+    _firestore = FirebaseFirestore.instance;
+    dataStream = _firestore.collection('NTUTLab321').doc(_data['machine']).snapshots();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('點滴尿袋智慧監控系統'),
         centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              padding: const EdgeInsets.all(8.0),
-              child: Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20.0),
-                ),
-                color: _data['change'] == '0'
-                    ? Colors.green
-                    : Colors.red,
-                elevation: 5.0,
-                child: ListTile(
-                  contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 12.0),
-                  leading: Icon(
-                    _data['change'] == '0'
-                        ? Icons.check_circle_outline
-                        : Icons.error_outline,
-                    color: Colors.white,
-                    size: 44.0,
-                  ),
-                  title: Text(
-                    _data['change'] == '0' ? '正常' : '待更換',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 24.0,
-                      color: Colors.white,
+      body: SingleChildScrollView(
+        child: StreamBuilder(
+          stream: dataStream,
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              print('@guest_page.dart -> snapshot.error -> ${snapshot.error.toString()}');
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.error_outline,
+                      color: Colors.lightBlue,
+                      size: 48.0,
                     ),
-                  ),
+                    SizedBox(height: 8.0),
+                    Text(
+                      '資料載入出現問題，請稍後再試',
+                      style: TextStyle(fontSize: 24.0),
+                    ),
+                    SizedBox(height: 20.0),
+                  ],
                 ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Divider(color: Colors.black),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SizedBox(width: 40.0),
-                Text(
-                  '床室號',
-                  style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
-                ),
-                SizedBox(width: 20.0),
-                Text(
-                  _data['judge'],
-                  style: TextStyle(fontSize: 20.0),
-                ),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Divider(color: Colors.black),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SizedBox(width: 40.0),
-                Text(
-                  '模    式',
-                  style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
-                ),
-                SizedBox(width: 20.0),
-                Text(
-                  _data['modedescription'],
-                  style: TextStyle(fontSize: 20.0),
-                ),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Divider(color: Colors.black),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SizedBox(width: 40.0),
-                Text(
-                  '電    量',
-                  style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
-                ),
-                SizedBox(width: 20.0),
-                CircleAvatar(
-                  backgroundColor: getPowerColor(_data['power']),
-                  child: Text(_data['power']),
-                ),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Divider(color: Colors.black),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SizedBox(width: 40.0),
-                Text(
-                  '工作記錄',
-                  style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
-                ),
-                SizedBox(width: 20.0),
-                Text(
-                  _data['time'],
-                  style: TextStyle(fontSize: 20.0),
-                ),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Divider(color: Colors.black),
-            ),
-          ],
+              );
+            } else {
+              if (snapshot.hasData) {
+                _data['alarm'] = snapshot.data['alarm'];
+                _data['change'] = snapshot.data['change'];
+                _data['modedescription'] = snapshot.data['modedescription'];
+                _data['power'] = snapshot.data['power'];
+                _data['time'] = snapshot.data['time'];
+                _data['title'] = snapshot.data['title'];
+                print(_data);
+              }
+              switch (snapshot.connectionState) {
+                case ConnectionState.active:
+                  return Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Card(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20.0),
+                            ),
+                            color: _data['change'] == '0'
+                                ? Colors.green
+                                : Colors.red,
+                            elevation: 5.0,
+                            child: ListTile(
+                              contentPadding: EdgeInsets.symmetric(
+                                  horizontal: 20.0, vertical: 12.0),
+                              leading: Icon(
+                                _data['change'] == '0'
+                                    ? Icons.check_circle_outline
+                                    : Icons.error_outline,
+                                color: Colors.white,
+                                size: 44.0,
+                              ),
+                              title: Text(
+                                _data['change'] == '0' ? '正常' : '待更換',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 24.0,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Divider(color: Colors.black),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            SizedBox(width: 40.0),
+                            Text(
+                              '床室號',
+                              style: TextStyle(
+                                  fontSize: 24.0, fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(width: 20.0),
+                            Text(
+                              _data['judge'],
+                              style: TextStyle(fontSize: 20.0),
+                            ),
+                          ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Divider(color: Colors.black),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            SizedBox(width: 40.0),
+                            Text(
+                              '模    式',
+                              style: TextStyle(
+                                  fontSize: 24.0, fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(width: 20.0),
+                            Text(
+                              _data['modedescription'],
+                              style: TextStyle(fontSize: 20.0),
+                            ),
+                          ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Divider(color: Colors.black),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            SizedBox(width: 40.0),
+                            Text(
+                              '電    量',
+                              style: TextStyle(
+                                  fontSize: 24.0, fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(width: 20.0),
+                            CircleAvatar(
+                              backgroundColor: getPowerColor(_data['power']),
+                              child: Text(_data['power']),
+                            ),
+                          ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Divider(color: Colors.black),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            SizedBox(width: 40.0),
+                            Text(
+                              '工作記錄',
+                              style: TextStyle(
+                                  fontSize: 24.0, fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(width: 20.0),
+                            Text(
+                              _data['time'],
+                              style: TextStyle(fontSize: 20.0),
+                            ),
+                          ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Divider(color: Colors.black),
+                        ),
+                      ],
+                    ),
+                  );
+                case ConnectionState.waiting:
+                  return Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SpinKitRing(color: Colors.lightBlue),
+                        SizedBox(height: 20.0),
+                        Text(
+                          '載入資料中...',
+                          style: TextStyle(fontSize: 24.0),
+                        ),
+                        SizedBox(height: 20.0),
+                      ],
+                    ),
+                  );
+                case ConnectionState.none:
+                  return Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.perm_scan_wifi_rounded,
+                          color: Colors.lightBlue,
+                          size: 48.0,
+                        ),
+                        SizedBox(height: 20.0),
+                        Text(
+                          '請檢查手機連線',
+                          style: TextStyle(fontSize: 24.0),
+                        ),
+                        SizedBox(height: 20.0),
+                      ],
+                    ),
+                  );
+                default:
+                  return Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.error_outline,
+                          color: Colors.lightBlue,
+                          size: 48.0,
+                        ),
+                        SizedBox(height: 8.0),
+                        Text(
+                          'Unexpected error',
+                          style: TextStyle(fontSize: 24.0),
+                        ),
+                        SizedBox(height: 20.0),
+                      ],
+                    ),
+                  );
+              }
+            }
+          },
         ),
       ),
     );
