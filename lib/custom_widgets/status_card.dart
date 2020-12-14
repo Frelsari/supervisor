fc707c7e8ef5146ea67688590c448d5e609e5bb9
Ricky Hu <ricky9667@gmail.com>
2020-12-14 16:11:37 +0800

    Remove redundant comments

--- lib/custom_widgets/status_card.dart
+++ lib/custom_widgets/status_card.dart
@@ -35,34 +35,3 @@ class StatusCard extends StatelessWidget {
     );
   }
 }
-
-/*
-Card(
-  shape: RoundedRectangleBorder(
-    borderRadius: BorderRadius.circular(20.0),
-  ),
-  color: _data['change'] == '0'
-      ? Colors.green
-      : Colors.red,
-  elevation: 5.0,
-  child: ListTile(
-    contentPadding: EdgeInsets.symmetric(
-        horizontal: 20.0, vertical: 12.0),
-    leading: Icon(
-      _data['change'] == '0'
-          ? Icons.check_circle_outline
-          : Icons.error_outline,
-      color: Colors.white,
-      size: 44.0,
-    ),
-    title: Text(
-      _data['change'] == '0' ? '正常' : '待更換',
-      style: TextStyle(
-        fontWeight: FontWeight.bold,
-        fontSize: 24.0,
-        color: Colors.white,
-      ),
-    ),
-  ),
-)
- */
