f5262945db9c3e076f1c5ad98fce0b6ed8afbac0
Ricky Hu <ricky9667@gmail.com>
2020-12-21 23:26:17 +0800

    Add comments in user_page

--- lib/pages/user_page.dart
+++ lib/pages/user_page.dart
@@ -1,3 +1,4 @@
+import 'package:firevisor/custom_widgets/message_screen.dart';
 import 'package:firevisor/pages/user_pages/supervisor.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -14,27 +15,31 @@ class User extends StatelessWidget {
     return BlocBuilder<AuthenticateBloc, AuthenticateState>(
       builder: (context, state) {
         if (state is AuthenticateLoggedInState) {
-          final Map userData = state.loginResult;
-          switch (userData['role']) {
-            case 'administrator':
+          final Map userData = state.loginResult; // 從 AuthenticateState 取得登入者資料
+          switch (userData['role']) { // 取得登入者權限
+            case 'administrator': // 最高權限
               return Administrator();
-            case 'staff':
+            case 'staff': // 醫護人員
               return Supervisor();
-            case 'guest':
-              return Guest(userData);
+            case 'guest': // 家屬
+              return Guest(userData); // 一併傳入登入資料
             default:
+              // 錯誤處理
               return Scaffold(
                 appBar: null,
-                body: Center(
-                  child: Text('ROLE ERROR'),
+                body: MessageScreen(
+                  child: Icon(Icons.error_outline),
+                  message: '使用者資訊出現問題',
                 ),
               );
           }
         } else {
+          // 錯誤處理
           return Scaffold(
             appBar: null,
-            body: Center(
-              child: Text('AUTH ERROR'),
+            body: MessageScreen(
+              child: Icon(Icons.error_outline),
+              message: '登入時出現問題',
             ),
           );
         }
